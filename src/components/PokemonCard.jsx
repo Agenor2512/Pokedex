9c5e3804f547e3853ee0d74fdb3106dc79289511
Agenor2512 <noemie.vasseur2512@gmail.com>
2023-12-12 15:37:02 +0100

    Added props to components

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,28 +1,16 @@
-const pokemonList = [
-    {
-        name: "Bulbasaur",
-        imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-
-    },
-    {
-        name: "Mew",
-    },
-];
-
-export default function PokemonCard() {
-    let pokemon = pokemonList[1];
-    
+/* eslint-disable react/prop-types */
+export default function PokemonCard({pokemon:{name, imgSrc}}) {
     return (
         <figure>
             {
-                pokemon.imgSrc ? 
-                <img src={pokemon.imgSrc}
+                // eslint-disable-next-line react/prop-types
+                imgSrc ? 
+                <img src={imgSrc}
                 alt="Bulbasaur" 
                 />
                 : <p>???</p>
             }
-            <figcaption>{pokemon.name}</figcaption>
+            <figcaption>{name}</figcaption>
         </figure>
     )
 }
